82217001a85cadbe8bd533773562130025b46cf3
carrot-123 <carolinewanggg@gmail.com>
2024-02-28 02:47:08 -0800

    rudimentary bubble search walkthrough

--- js/app.js
+++ js/app.js
@@ -6,15 +6,37 @@ export default class App {
     this.workingArray1 = ["2", "1", "3"];
     this.sortedArray2 = ["1", "2", "3"];
     this.workingArray2 = ["3", "2", "1"];
+    this.sortedArray = ["1", "2", "3", "4", "5", "6"];
+    this.workingArray = ["6", "3", "4", "5", "2", "1"];
     this.steps = [
       ["1", "2"],
       ["2", "3"],
     ];
+    this.bubbleSortSteps = [
+      ["6", "3"],
+      ["6", "4"],
+      ["6", "5"],
+      ["6", "2"],
+      ["6", "1"],
+      ["5", "2"],
+      ["5", "1"],
+      ["4", "2"],
+      ["4", "1"],
+      ["3", "2"],
+      ["3", "1"],
+      ["1", "2"],
+    ];
+
     this.workingArray3 = ["3", "1", "2"];
     this._freeSort1 = new FreeSort(this.sortedArray1, this.workingArray1, "1");
     this._freeSort2 = new FreeSort(this.sortedArray2, this.workingArray2, "2");
-    this._freeSort3 = new RestrictedSort(this.steps, this.workingArray3, "3");
+    //this._freeSort3 = new RestrictedSort(this.steps, this.workingArray3, "3");
+    this._bubbleSort = new RestrictedSort(
+      this.bubbleSortSteps,
+      this.workingArray,
 
+      "0"
+    );
     this._nextPage = this._nextPage.bind(this);
     document.querySelector("#next1").addEventListener("click", this._nextPage);
     document.querySelector("#next2").addEventListener("click", this._nextPage);
--- js/restrictedSort.js
+++ js/restrictedSort.js
@@ -4,16 +4,61 @@ export default class RestrictedSort {
     this.selectedNums = [];
     this.steps = steps; // check which step we are on...
     this.workingArray = workingArray;
+    this.number = sortNumber;
     this.sortNumber = ".sort" + sortNumber;
     this._handleSwap = this._handleSwap.bind(this);
     document
       .querySelectorAll(this.sortNumber)
       .forEach((box) => box.addEventListener("click", this._handleSwap));
-    this.index = 0;
+    this._stepOne = this._stepOne.bind(this);
+    document.querySelector("#step").addEventListener("click", this._stepOne);
+    this.stepsIndex = 0;
+    this.posIndex = 1;
+    this.swapIndex = 2;
+    this.posUpperBound = 5;
+  }
+  _stepOne() {
+    this.selectedNums = [];
+    let posVal = document.getElementById(
+      "sort" + this.number + "box" + this.posIndex
+    ).children[0].textContent;
+    let swapVal = document.getElementById(
+      "sort" + this.number + "box" + this.swapIndex
+    ).children[0].textContent;
+
+    if (
+      this.steps[this.stepsIndex].indexOf(posVal) === -1 ||
+      this.steps[this.stepsIndex].indexOf(swapVal) === -1
+    ) {
+      if (this.posIndex === this.posUpperBound) {
+        // if you are not supposed to swap the current values, move on
+        // the steps below are only true for bubble sort
+        this.posIndex = 1;
+        this.swapIndex = 2;
+        this.posUpperBound -= 1;
+      } else {
+        this.posIndex += 1;
+        this.swapIndex += 1;
+      }
+    } else {
+      console.log("you are supposed to swap the current values");
+    }
+    console.log(this.posIndex);
+    console.log(this.swapIndex);
   }
   _handleSwap(event) {
     // after x amount of steps, give option to move on
-    if (this.index < this.steps.length) {
+    let targetId = event.target.id;
+    console.log(targetId);
+    console.log("sort" + this.number + "box" + this.posIndex);
+    let posVal = document.getElementById(
+      "sort" + this.number + "box" + this.posIndex
+    ).children[0].textContent;
+    let swapVal = document.getElementById(
+      "sort" + this.number + "box" + this.swapIndex
+    ).children[0].textContent;
+
+    if (this.stepsIndex < this.steps.length) {
       let targetId = event.target.id;
       let value = document.getElementById(targetId).children[0];
       if (this.selectedNums.length === 0) {
@@ -21,13 +66,18 @@ export default class RestrictedSort {
       } else if (this.selectedNums[0] === value) {
         this.selectedNums.pop();
       } else if (
-        this.steps[this.index].indexOf(this.selectedNums[0].textContent) ===
-          -1 ||
-        this.steps[this.index].indexOf(value.textContent) === -1
+        // need to make sure that the current positions have been selected
+        this.steps[this.stepsIndex].indexOf(
+          this.selectedNums[0].textContent
+        ) === -1 ||
+        this.steps[this.stepsIndex].indexOf(value.textContent) === -1
       ) {
         console.log("invalid move"); // highlight the hint/the correct elements to swap
         this.selectedNums = [];
-      } else {
+      } else if (
+        this.steps[this.stepsIndex].indexOf(posVal) > -1 &&
+        this.steps[this.stepsIndex].indexOf(swapVal) > -1
+      ) {
         this.selectedNums.push(value);
         let child1 = this.selectedNums[0];
         let parent1 = child1.parentElement;
@@ -37,10 +87,11 @@ export default class RestrictedSort {
         parent2.prepend(child1);
         this.selectedNums = [];
 
-        if (this.index === this.workingArray.length - 1) {
+        console.log(this.steps);
+        if (this.stepsIndex === this.steps.length - 1) {
           console.log("sorted!");
         } else {
-          this.index += 1;
+          this.stepsIndex += 1;
         }
       }
     }
